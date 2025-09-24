790b0f8a0467055d71c3f25183b2bfa94e06aa1a
Elay Sason <elaysason@gmail.com>
2025-09-24 20:29:02 +0300

    implement dynamic grid creation with user-defined size and random colors, and get more opaque on hover

--- etch-a-sketch-scrpit.js
+++ etch-a-sketch-scrpit.js
@@ -1,11 +1,42 @@
 const flexConatiner = document.querySelector(".flex-container");
-console.log(flexConatiner);
 
-for (let i = 0; i < 256; i++) {
-  let squere_div = document.createElement("div");
-  squere_div.addEventListener("mouseover", () => {
-    squere_div.style.backgroundColor = "orange";
-  });
-  flexConatiner.appendChild(squere_div);
+function getRandomRGB() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
 }
-console.log(flexConatiner);
+
+function createGrid(size) {
+  while (flexConatiner.firstChild) {
+    flexConatiner.removeChild(flexConatiner.firstChild);
+  }
+  for (let i = 0; i < size * size; i++) {
+    let squere_div = document.createElement("div");
+    squere_div.style.opacity = "0.0";
+    squere_div.addEventListener("mouseover", () => {
+      let currentOpacity = parseFloat(squere_div.style.opacity);
+      if (currentOpacity < 1) {
+        let newOpacity = Math.min(currentOpacity + 0.1, 1);
+        squere_div.style.opacity = newOpacity.toString();
+      }
+    });
+    squere_div.style.backgroundColor = getRandomRGB();
+    let squareSize = 960 / size;
+    squere_div.style.width = `${squareSize}px`;
+    squere_div.style.height = `${squareSize}px`;
+    console.log(squere_div.style.width);
+    flexConatiner.appendChild(squere_div);
+  }
+}
+createGrid(16);
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  let userRow = prompt("Enter number of rows (max 100):");
+  if (userRow <= 100) {
+    createGrid(userRow);
+  } else {
+    alert("Number must be less than or equal to 100");
+    createGrid(16);
+  }
+});
